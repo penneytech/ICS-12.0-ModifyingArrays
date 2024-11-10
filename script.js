8b2d472d3fee77ac457fe2bc3c10c5d01c73680d
penneytech <99091373+penneytech@users.noreply.github.com>
2024-11-10 18:00:45 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Follow the instructions to modify or parse the following arrays.
+// Log each result to the console. 
+
+// 1) Use slice() to create an array containing "Milk, Eggs"
+const fridge = ["Radishes", "Milk", "Eggs", "Celery", "Apples"];
+
+// 2) Use splice() to add the elements "Tires" and "Paint" directly after the "Engine" element. 
+const car = ["Steering Wheel", "Engine", "Oil", "Gas"]; 
+
+// Now, use splice() again to remove the word "Steering Wheel"
+
+
+// 3) Use pop() to remove the last element of this array: 
+const colors = ["Red", "Orange", "Yellow", "Brown"];
+
+// 4) Use push() to add a new color to the end of this array: 
+const colors2 = ["Blue", "Purple", "Green", "Pink"];
+
+// 5) Use indexOf() to determine the index number of "Lego Batman"
+const shows = ["Survivor", "Becoming Anna", "Hannah Montana", "Lego Batman"];
+
+// 6) Use from() to create an array from the word "Salad"
+
+
+// 7) Use includes() to determine of this array includes the letter "Z"
+const animal = ["z", "e", "b", "r", "a"];
+
